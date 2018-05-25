8f8d3d38cfb6270cc1ef6ae763f5fc6fd1dcd60a
ktjbrowne <ktjbrowne@gmail.com>
2018-05-25 02:35:01 +0100

    Update krt-install.sh

--- krt-install.sh
+++ krt-install.sh
@@ -34,19 +34,10 @@ clear
 
 
 echo "
-    ___T_
-   | o o |
-   |__-__|
-   /| []|\\
- ()/|___|\()
-    |_|_|
-    /_|_\  ------- MASTERNODE INSTALLER v2.6 -------+
- |                                                |
- | HTRCv1.0.2.0-60031 Installer by Kurbz          |
- | for Ununtu 16.04 only                          |
- |                                                |::
- +------------------------------------------------+::
-   ::::::::::::::::::::::::::::::::::::::::::::::::::
+ |   +------- MASTERNODE INSTALLER v1.1 -------+  |
+ |   KRT Installer by Kurbz                       |
+ |   for Ununtu 16.04 only                        |
+ +------------------------------------------------+
 "
 
 sleep 2
@@ -106,92 +97,78 @@ yes | ufw enable
 
 
 echo "
-**********Installing deamon***********
-"
-sleep 2
-wget https://github.com/htrcoin/htrcoin/releases/download/v1020/HTRC_Headless_Linux_v1020.zip
-unzip HTRC_Headless_Linux_v1020.zip
-cp /root/linux/hightemperatured /usr/local/bin
-chmod +x /usr/local/bin/hightemperatured 
-cp /root/linux/hightemperatured /root/
-chmod +x /usr/local/bin/hightemperatured 
 
-echo "
-*********Configuring confs***********
+**********Installing deamon***********
+
 "
 sleep 2
-mkdir $USERHOME/.HighTemperature
+wget  https://github.com/kreita-pro/krt/releases/download/v1.2.2.3/krtd-Linux64
+cp /root/krtd-Linux64 /usr/local/bin/krtd
+cp /root/krtd-linux64 /root/krtd
+chmod +x /usr/local/bin/krtd
+chmod +x /root/krtd
+echo "
+
+*********Configuring confs***********
+
+"
+sleep 2
+mkdir $USERHOME/.krt
 
 # Create hightemperature.conf
-touch $USERHOME/.HighTemperature/HighTemperature.conf
-cat > $USERHOME/.HighTemperature/HighTemperature.conf << EOL
+touch $USERHOME/.krt/krt.conf
+cat > $USERHOME/.krt/krt.conf << EOL
 rpcuser=${RPCUSER}
 rpcpassword=${RPCPASSWORD}
 rpcallowip=127.0.0.1
-rpcport=11369
-port=11368
+rpcport=47048
+port=47047
 listen=1
 server=1
 daemon=1
-gen=1
+listenonion=0
 logtimestamps=1
 maxconnections=256
 externalip=${IP}
-bind=${IP}:11368
+bind=${IP}:47047
 masternodeaddr=${IP}
 masternodeprivkey=${KEY}
 masternode=1
-addnode=45.76.37.60:11368
-addnode=1.160.157.57:11368
-addnode=186.113.245.245:11368
-addnode=85.247.171.175:11368
-addnode=83.213.187.72:11368
-addnode=104.168.52.146:11368
-addnode=98.24.33.121:11368
-addnode=79.184.123.19:11368
-addnode=34.207.125.26:11368
-addnode=45.77.147.113:11368
-addnode=149.28.32.21:11368
-addnode=107.191.58.52:11368
-addnode=159.65.66.83:11368
-addnode=104.238.167.55:11368
-addnode=46.160.207.24:11368
-addnode=188.27.201.93:11368
-addnode=149.28.74.224:11368
-addnode=95.216.33.107:11368
-addnode=54.37.232.179:11368
-addnode=108.160.130.198:11368
+
 EOL
-chmod 0600 $USERHOME/.HighTemperature/HighTemperature.conf
-chown -R $USER:$USER $USERHOME/.HighTemperature
+chmod 0600 $USERHOME/.krt/krt.conf
+chown -R $USER:$USER $USERHOME/.krt
 
 sleep 1
 
-cat > /etc/systemd/system/hightemperatured.service << EOL
+cat > /etc/systemd/system/krtd.service << EOL
 [Unit]
-Description=hightemperatured
+Description=krtd
 After=network.target
 [Service]
 Type=forking
 User=${USER}
 WorkingDirectory=${USERHOME}
-ExecStart=/usr/local/bin/hightemperatured -conf=${USERHOME}/.HighTemperature/HighTemperature.conf -datadir=${USERHOME}/.HighTemperature
-ExecStop=/usr/local/bin/hightemperatured -conf=${USERHOME}/.HighTemperature/HighTemperature.conf -datadir=${USERHOME}/.HighTemperature stop
+ExecStart=/usr/local/bin/krtd -conf=${USERHOME}/.krt/krt.conf -datadir=${USERHOME}/.krt
+ExecStop=/usr/local/bin/krtd -conf=${USERHOME}/.krt/krt.conf -datadir=${USERHOME}/.krt stop
 Restart=on-abort
 [Install]
 WantedBy=multi-user.target
 EOL
 
-chmod +x /usr/local/bin/hightemperatured 
+chmod +x /usr/local/bin/krtd 
 sudo ln -s /usr/lib/x86_64-linux-gnu/libboost_system.so.1.58.0 /usr/lib/x86_64-linux-gnu/libboost_program_options.so.1.54.0
 #start service
 echo "
+
 ********Starting Service*************
+
+
 "
 sleep 3
-sudo systemctl enable hightemperatured
-sudo systemctl start hightemperatured
-sudo systemctl start hightemperatured.service
+sudo systemctl enable krtd
+sudo systemctl start krtd
+sudo systemctl start krtd.service
 
 #clear
 
@@ -201,7 +178,7 @@ echo "Service Started... Press any key to continue. "
 
 echo "Your masternode is syncing. Please wait for this process to finish. "
 
-until su -c "hightemperatured masternode status 2>/dev/null | grep 'Masternode Running Remotly' > /dev/null" $USER; do
+until su -c "krtd masternode status 2>/dev/null | grep 'Masternode Running Remotly' > /dev/null" $USER; do
   for (( i=0; i<${#CHARS}; i++ )); do
     sleep 5
     #echo -en "${CHARS:$i:1}" "\r"
@@ -209,19 +186,19 @@ until su -c "hightemperatured masternode status 2>/dev/null | grep 'Masternode R
     echo "Service Started. Your masternode is syncing. 
     When Current = Synced then select your MN in the local wallet and start it."
     echo "Current Block: "
-    su -c "curl http://explorer.htrcoin.com/api/getblockcount" $USER
+    su -c "curl http://explorer.kreita.io/api/getblockcount" $USER
     echo "Synced Blocks: "
-    su -c "hightemperatured getblockcount" $USER
+    su -c "krtd getblockcount" $USER
   done
 done
 
-su -c "/usr/local/bin/hightemperatured masternode status" $USER
+su -c "/usr/local/bin/krtd masternode status" $USER
 
 #sleep 1
-#su -c "/usr/local/bin/hightemperatured masternode start" $USER
+#su -c "/usr/local/bin/krtd masternode start" $USER
 #sleep 1
 #clear
-#su -c "/usr/local/bin/hightemperatured masternode status" $USER
+#su -c "/usr/local/bin/krtd masternode status" $USER
 #sleep 5
 
 #echo "" && echo "Masternode setup completed." && echo ""
